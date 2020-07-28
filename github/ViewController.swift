229ab4236ce4d46f4b497243fd200248c092a8e6
elisa cruz <elisasmc216@gmail.com>
2020-07-28 14:05:10 -0400

    pLS WORK COMMIT

--- github/ViewController.swift
+++ github/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view. yeah lol!! all the homies hate github
+        // Do any additional setup after loading the view. yeah lol!! all the homies hate github BRUH
     }
 
 
